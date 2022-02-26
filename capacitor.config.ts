0b2da15899299b924da0007ad1d5e03b6e4b3f7d
ridwanmalik <skredonemalik@gmail.com>
2022-02-27 01:19:10 +0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'dhar-nea-choli',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
